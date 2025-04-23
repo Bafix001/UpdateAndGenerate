64d0cd986230c2b32572150b304677d224fc5a0c
Bafix001 <zibooumorou44@gmail.com>
2025-04-23 09:42:23 +0200

    avant deploiement

--- client/vue.config.js
+++ client/vue.config.js
@@ -1,13 +1,23 @@
 module.exports = {
-  devServer: {
+    // Configuration existante du proxy
+    devServer: {
       port: 8080,
       proxy: {
-          '/api': {
-              target: 'http://localhost:3000',
-              changeOrigin: true,
-              pathRewrite: { '^/api': '' },
-              secure: false
-          }
+        '/api': {
+          target: 'http://localhost:3000',
+          changeOrigin: true,
+          pathRewrite: { '^/api': '' },
+          secure: false
+        }
       }
+    },
+  
+    // Nouvelle configuration pour le titre
+    chainWebpack: config => {
+      config.plugin('html').tap(args => {
+        args[0].title = "Update & Generate" // Titre par défaut
+        return args
+      })
+    }
   }
-};
+  
